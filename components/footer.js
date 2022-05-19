e6f818332032506be5dfa42c044e0c5000c2c67e
Roma <rossraijin@gmail.com>
2022-05-19 13:18:28 +0300

    19-05-R

--- components/footer.js
+++ components/footer.js
@@ -47,11 +47,6 @@ footerTemplate.innerHTML = `
       <li><a href="work.html">Work</a></li>
       <li><a href="contact.html">Contact</a></li>
     </ul>
-    <ul class="social-row">
-      <li><a href="https://github.com/my-github-profile"><i class="fab fa-github"></i></a></li>
-      <li><a href="https://twitter.com/my-twitter-profile"><i class="fab fa-twitter"></i></a></li>
-      <li><a href="https://www.linkedin.com/in/my-linkedin-profile"><i class="fab fa-linkedin"></i></a></li>
-    </ul>
   </footer>
 `;
 
