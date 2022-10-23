e8e9b0f8c4540ab8143533b31a0e69312e0dac4a
Svyatoslav <savchuksr@gmail.com>
2022-10-23 10:49:55 +0300

    23.10

--- scripts/js-lessons/practice/practice-4.js
+++ scripts/js-lessons/practice/practice-4.js
@@ -2,12 +2,52 @@
 // функція збільшує його вдвічі та повертає, якщо ні - результат НЕ ПОВЕРТАЄ, 
 // а лише виводить у консоль це число.
 
+const number1 = 30;
+const number2 = 9;
+const number3 = 8;
+const number4 = 10;
 
 
-
+function multipleofthree(x) {
+    if (x%3 === 0) {
+        return x * 2;
+    } else {
+        console.log(x);
+    }
+}
+console.log(multipleofthree(number1));
+console.log(multipleofthree(number2));
+console.log(multipleofthree(number3));
+console.log(multipleofthree(number4));
 // Завдання №2. Написати функцію з використанням switch-case. 
 // Функція приймає число (1, 2 або 3), якщо це 1 - збільшує вдвічі і повертає, 
 // якщо 2 - просто повертає його, якщо 3 - перетворює на строку та повертає.
+
+
+
+function swcase1 (w) {
+    let result1;
+    switch (w) {
+        case 1:
+            result1 = 1 * 2;
+            break;
+
+        case 2:
+            result1 = 3;
+            break;
+
+        case 3:
+            result1 = String(3);
+            break;    
+
+        default:
+            console.log("не получилось");
+            break;
+    }
+    return result1;
+}
+
+console.log(swcase1(1));
 
 
 function example (k) {
