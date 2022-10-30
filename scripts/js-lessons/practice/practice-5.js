e372176691393f1fc4ca010c1f261942dc0015dd
Roma <rossraijin@gmail.com>
2022-10-30 16:59:07 +0200

    30-10

--- scripts/js-lessons/practice/practice-5.js
+++ scripts/js-lessons/practice/practice-5.js
@@ -1,34 +1,56 @@
 // 1. Написати функцію, яка приймає число, якщо воно більше 20, то повертаємо йогo,
 // а якщо менше 20 - то виводимо його в консоль, зменшуємо на 2 і виводимо результат
 // у консоль поки результат більше або дорівнює 4.
-// Тобто має вивести у консоль 20, 18, 16, 14, 12, 10, 8, 6, 4
+// Тобто має вивести у консоль 20, 18, 16, 14, 12, 10, 8, 6, 4, 2
 
 // Щоб зменшити число на 2, x=x-2, або 2 рази x--
 
-let count = 20;
-while (count > 3) {
-    if (count > 20) {
-          // функція пряцює но що тут написати щоб повернуло результат я не знаю return не працює
-    }
-    if (count < 21) {
-        if (count > 3) {
-            console.log(count);
-            count--;
-            count--;
+function func1 (x) {
+    if (x > 20) {
+        return x;
+    } else {
+        console.log('x: ', x);
+        let result = x;
+        while (result >= 4) {
+            result = result - 2;
+            console.log('r ===>', result);
         }
+        return result;
     }
 }
+
+const y = func1(20);
+console.log(y);
+
+
+// let count = 20;
+// while (count > 3) {
+//     if (count > 20) {
+//           // функція пряцює но що тут написати щоб повернуло результат я не знаю return не працює
+//     }
+//     if (count < 21) {
+//         if (count > 3) {
+//             console.log(count);
+//             count--;
+//             count--;
+//         }
+//     }
+// }
 
 
 
 // 2. В нас є масив "arr", перебрати увесь масив, та збільшити кожне число на 3, УСІ получені
 // результати просумувати, результат зберегти у змінну
 const arr = [1, 2, 3, 4, 5, 6, 7];
+let start = 0;
 for (let index = 0; index < arr.length; index++) {
     const element = arr[index];
-    const g = element * 3 ; 
-    console.log("x3+",g)
+    const g = element + 3; 
+    console.log("x3+", g);
+    // start = start + g; // 1й спосіб
+    start+=g; // 2й спосіб
 }
+console.log('result: ', start);
 
 
 // 3. В нас є об'єкт "obj". Перебрати об'єкт та збільшити усі значення вдвічі.
@@ -40,7 +62,7 @@ const obj = {
 for (let x in obj) {
     obj[x] = obj[x] * 2;
 }
-console.log(obj);0
+console.log(obj);
 
 // Приклад до завдання 3, розібрати уважно, тому що по факту це рішення, тут збільшення в 10
 const obj2 = {
---
+++ scripts/js-lessons/theory/lesson-10.Continue-Break.js
@@ -0,0 +1,56 @@
+// Інструкції переходу -- break
+
+// const x = 10;
+// const arr = [3, 'fgh', null, 8, 10, 'a', 90]; // 7 ітерацій
+
+// for (let i = 0; i < arr.length; i++) {
+//     const el = arr[i];
+//     if (el === 10) {
+//         console.log('el: ', el);
+//         break;
+//     }
+//     console.log('el222222: ', el);
+// }
+
+// continue
+// const arr2 = [5, 10, 15, 20, 50, 100];
+// for (let j = 0; j < arr2.length; j++) {
+//     const g = arr2[j];
+//     if (g < 50) {
+//         console.log('res: ', g * 2);
+//         continue;
+//     }
+
+//     const x = 100;
+//     console.log('g: ', g * x);
+// }
+
+
+const obj = {
+    x: 1,
+    y: 2,
+    z: {
+        cat: 'vasya'
+    },
+    p: 23
+}
+
+// Uncaught ReferenceError: x is not defined - це значить що інтерпретатору невідомо що таке х
+
+console.log(obj['x']);
+console.log(obj.z.cat);
+
+console.log('index: ', 'polk'.indexOf('k'))
+const date = new Date();
+console.log('date: ', date);
+
+// console.log(obj.p) // undefined
+// console.log(obj.p.f); // Uncaught TypeError: Cannot read properties of undefined (reading 'f')
+if (obj.hasOwnProperty('p')) {
+    console.log('p: ', obj.p);
+} else {
+    console.log('no');
+}
+
+obj.k = 45;
+console.log('obj: ', obj);
