f345fa0d596bbcb55fccf65519382809cbe1643b
Roma <rossraijin@gmail.com>
2022-11-06 13:31:31 +0200

    l11

---
+++ scripts/js-lessons/theory/lesson-11.Variables.Ext.js
@@ -0,0 +1,20 @@
+// Примітивні: string, number, boolean, null, undefined
+// Об'єкти: Object, Array, Function
+
+// https://medium.com/webbdev/js-49122c7d8c26
+
+
+// Змінна за посиланням чи за значенням.
+let myStr1 = '111'; // перша ячейка пам'яті
+let myStr2 = myStr1; // друга ячейка пам'яті
+myStr2 = '222'; // Uncaught TypeError: Assignment to constant variable. - помилка, бо не можна переназначати константу
+console.log(myStr1); // 111
+console.log(myStr2); // 222
+
+
+let var1 = {name: 'Jim'}; // перша ячейка пам'яті
+let var2 = var1; // та ж сама перша ячейка пам'яті
+var2.name = 'John';
+
+console.log(var1); // Jim 
+console.log(var2); // John
